4f39d70a319cc67acd887ad3f262bf232921ff87
lodenos <nikolaiilodenos@gmail.com>
2020-02-05 15:41:47 +0000

    reoginize the code

--- rover.py
+++ rover.py
@@ -1,5 +1,3 @@
-import sys
-
 class Rover:
   __mapLeft = {
     'N': 'W',
@@ -47,21 +45,3 @@ class Rover:
 
   def interpreter(self, instruction):
     [self.__instruction[index](self) for index in instruction]
-
-if __name__ == '__main__':
-  if len(sys.argv) < 2:
-    print "Argument Error"
-    exit(-1)
-  with open(sys.argv[1]) as file:
-    data = file.read().split('\n')
-    data = [data[2::2], data[1::2]]
-    for index in range(len(data[0])):
-      instruction = data[0][index]
-      position = data[1][index].split(" ")
-      if len(position) != 3:
-        print "Error Position"
-        exit(-1)
-      rover = Rover(int(position[0]), int(position[1]), position[2])
-      rover.interpreter(instruction)
-      print(rover)
-      del rover
